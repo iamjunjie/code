e7d3a7de0952547fc85f4f0d47be139b0ae2d18c
iamjunjie <1252547929@qq.com>
2021-04-29 15:56:45 +0800

    代码优化

--- php/tree.php
+++ php/tree.php
@@ -1,118 +1,179 @@
 <?php
+
 /**
- * 树
+ * 公共函数库-数据树
  * 
- * @author wangjunjie <1252547929@qq.com>
- * @date 2015-06-16
+ * 1. 作用是将数据结果集处理成树状结构
+ * 2. 使用方法如下
+ * $lists = \DataTree::getTree([
+ * 	'data' => $lists, 
+ * 	'pid' => 'pid', 
+ * 	'id' => 'id', 
+ * 	'name' => 'name', 
+ * 	'char' => getArrVal($data, 'tree_char'), 
+ * 	'style' => getArrVal($data, 'tree_style')
+ * ]);
+ * 3. 注意name、char、style 这三个字段是配合使用的
+ * 
+ * @author wuming 2021-04-29
  */
-class Tree {
+class DataTree
+{
+	/**
+	 * 原始数据
+	 * 
+	 * @var array
+	 */
+	private static $data = [];
 
-	//原始数据
-	private static $data = array();
-
-	//父级字段名
+	/**
+	 * 父字段名
+	 * 
+	 * @var string
+	 */
 	private static $pid = null;
 
-	//主键字段名
+	/**
+	 * 主键字段名
+	 * 
+	 * @var string
+	 */
 	private static $id = null;
 
-	//文本字段名
+	/**
+	 * 文本字段名
+	 * 
+	 * @var string 
+	 */
 	private static $name = null;
 
-	//填充字符
+	/**
+	 * 填充字符
+	 * 
+	 * @var string
+	 */
 	private static $char = '—';
 
-	//树的风格
+	/**
+	 * 树的风格
+	 * 
+	 * 1. 名字根据层级填充设置的填充字符，适用于select下拉框
+	 * 2. 树状结构，叶子数据包含在subs里
+	 * 
+	 * @var int
+	 */
 	private static $style = 2;
 
-	//结果数据
-	private static $tree = array();
+	/**
+	 * 结果数据
+	 * 
+	 * @var array
+	 */
+	private static $tree = [];
 
 	/**
 	 * 获取树
 	 * 
-	 * @param  array $params 参数
-	 * 	                     data  原始数据
-	 * 	                     pid   父级字段名
-	 * 	                     id    主键字段名
-	 * 	                     name  文本字段名
-	 * 	                     char  填充字符
-	 * 	                     style 风格（1:适用于select框，2:数组）
+	 * @param string $params['id'] 主键字段名
+	 * @param string $params['pid'] 父节点字段名
+	 * @param array $params['data'] 数据
+	 * @param string $params['name'] 文本字段名
+	 * @param string $params['char'] 填充字符串，默认-
+	 * @param string $params['style'] 风格(1:试用与select下拉框，2:数组)
+	 * 
 	 * @return array
+	 * 
+	 * @author wuming 2021-04-29
 	 */
-	public static function getTree($params){
-		self::$tree  = array();
-		self::$data  = $params['data'];
-		self::$pid   = $params['pid'];
-		self::$id    = $params['id'];
-		self::$name  = $params['name'];
-		self::$char  = isset($params['char']) ? $params['char'] : self::$char;
-		self::$style = isset($params['style']) ? $params['style'] : self::$style;
-		$root = self::getRoot();
-		if(empty($root)){ return self::$data; }
-		foreach ($root as $key => $value) {
-			$level          = 1;
-			$value['depth'] = 0;
-			switch (self::$style) {
-				case 1:
-					self::$tree[] = $value;
-					self::buildLeaf($value, $level);
-					break;
-				case 2:
-					$value['sublist'] = array();
-					self::buildLeaf($value, $level);
-					self::$tree[] = $value;
-					break;
-				default:
-					self::$tree[] = $value;
-					self::buildLeaf($value, $level);
-					break;
+	public static function getTree($params)
+	{
+		self::$id = $params['id'];
+		self::$pid = $params['pid'];
+		self::$data = $params['data'];
+		self::$name = getArrVal($params, 'name');
+		$char = getArrVal($params, 'char');
+		self::$char = $char ? $char : self::$char;
+		$style = getArrVal($params, 'style');
+		self::$style = $style ? $style : self::$style;
+		// 获取数据结果集中的根数据
+		if ($root = self::getRoot()) {
+			// 找寻根下的叶子数据
+			foreach ($root as $value) {
+				$level = 1;
+				$value['depth'] = 0;
+				switch (self::$style) {
+					case 1:
+						self::$tree[] = $value;
+						self::buildLeaf($value, $level);
+						break;
+					case 2:
+						$value['subs'] = [];
+						self::buildLeaf($value, $level);
+						self::$tree[] = $value;
+						break;
+					default:
+						self::$tree[] = $value;
+						self::buildLeaf($value, $level);
+						break;
+				}
 			}
+
+			return self::$tree;
 		}
-		return self::$tree;
+
+		return self::$data;
 	}
 
 	/**
 	 * 获取根
 	 * 
 	 * @return array
+	 * 
+	 * @author wuming 2021-04-29
 	 */
-	private static function getRoot(){
-		$root = array();
+	private static function getRoot()
+	{
+		$root = [];
 		foreach (self::$data as $key => $value) {
-			if(empty($value[self::$pid])){
+			if (empty($value[self::$pid])) {
 				unset(self::$data[$key]);
 				$root[] = $value;
 			}
 		}
+
 		return $root;
 	}
 
 	/**
 	 * 构建叶子
 	 * 
-	 * @param  array $item  数据项
-	 * @param  int   $level 层级数
+	 * @param mixed $item 数据项
+	 * @param int $level 层级数
+	 * 
+	 * @author wuming 2021-04-29
 	 */
-	private static function buildLeaf(&$item, $level){
-		if(!empty(self::$data)){
+	private static function buildLeaf(&$item, $level)
+	{
+		if (self::$data) {
 			foreach (self::$data as $key => $value) {
 				$value['depth'] = $level;
-				if($value[self::$pid] != $item[self::$id]){ continue; }
+				if ($value[self::$pid] != $item[self::$id]) {
+					continue;
+				}
 				unset(self::$data[$key]);
 				switch (self::$style) {
 					case 1:
-						$value[self::$name] = str_repeat(self::$char, $level*2) . $value[self::$name];
+						self::$name && $value[self::$name] = str_repeat(self::$char, $level * 2) . $value[self::$name];
 						self::$tree[] = $value;
 						self::buildLeaf($value, $level + 1);
 						break;
 					case 2:
-						$value['sublist'] = array();
+						$value['subs'] = [];
 						self::buildLeaf($value, $level + 1);
-						array_push($item['sublist'], $value);
+						array_push($item['subs'], $value);
 						break;
 					default:
-						$value[self::$name] = str_repeat(self::$char, $level*2) . $value[self::$name];
+						self::$name && $value[self::$name] = str_repeat(self::$char, $level * 2) . $value[self::$name];
 						self::$tree[] = $value;
 						self::buildLeaf($value, $level + 1);
 						break;
@@ -121,38 +182,3 @@ class Tree {
 		}
 	}
 }
-
-$data = array(
-	array('parent_id' => 0, 'id' => 1, 'name' => '北京', 'address' => ''),
-	array('parent_id' => 1, 'id' => 2, 'name' => '海淀', 'address' => ''),
-	array('parent_id' => 1, 'id' => 3, 'name' => '朝阳', 'address' => ''),
-	array('parent_id' => 0, 'id' => 4, 'name' => '河北', 'address' => ''),
-	array('parent_id' => 4, 'id' => 5, 'name' => '邯郸', 'address' => ''),
-	array('parent_id' => 4, 'id' => 6, 'name' => '唐山', 'address' => ''),
-	array('parent_id' => 2, 'id' => 7, 'name' => '海淀1', 'address' => ''),
-	array('parent_id' => 2, 'id' => 8, 'name' => '海淀2', 'address' => ''),
-	array('parent_id' => 3, 'id' => 9, 'name' => '朝阳1', 'address' => ''),
-	array('parent_id' => 3, 'id' => 10, 'name' => '朝阳2', 'address' => ''),
-);
-
-$one_style = Tree::getTree(array(
-	'data' => $data,
-	'pid'  => 'parent_id',
-	'id'   => 'id',
-	'name' => 'name',
-	'char' => '—',
-	'style' => 1,
-));
-
-$two_style = Tree::getTree(array(
-	'data' => $data,
-	'pid'  => 'parent_id',
-	'id'   => 'id',
-	'name' => 'name',
-	'style' => 2,
-));
-
-printf('<meta charset="utf-8" /> <pre>风格一：%s</pre> <pre>风格二：%s<pre> <a href="%s" target="_black">查看源代码</a>', 
-	print_r($one_style, true), print_r($two_style, true), 
-	"//{$_SERVER['HTTP_HOST']}/code.php?vsc=" . urlencode(__FILE__)
-);
