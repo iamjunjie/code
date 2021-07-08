c163b66c43cae55736a7c59f392835a832633cb7
iamjunjie <1252547929@qq.com>
2021-07-08 13:22:46 +0800

    新增备份数据库结构脚本

---
+++ php/backupdb.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * 一键备份数据库结构脚本
+ * 
+ * 1. 进入目录：cd code/php
+ * 2. 执行脚本：php backupdb.php
+ */
+
+use Think\Db\Driver\Mysql;
+
+// 引入MySQL操作类
+require 'mysql.php';
+
+// 数据库配置
+$dbconf = [
+    'vclass_base' => [
+        'hostname' => '192.168.15.104',
+        'hostport' => '13361',
+        'database' => 'vclass_base',
+        'username' => 'webdev',
+        'password' => 'webdev',
+    ],
+    'vclass_osc' => [
+        'hostname' => '192.168.15.104',
+        'hostport' => '13361',
+        'database' => 'vclass_osc',
+        'username' => 'webdev',
+        'password' => 'webdev',
+    ],
+];
+
+foreach ($dbconf as $key => $value) {
+    // 数据库SQL文件名
+    $filename = $key . '.sql';
+    // 清空文件内容
+    file_put_contents($filename, '');
+    // 创建操作数据库类对象
+    $db = MySqlDB::getInstance($value);
+    // 查询数据库所有表
+    $tables = $db->getTables();
+    $totals = count($tables);
+    foreach ($tables as $tk => $tv) {
+        printf("数据库{$key}处理进度：%s/%s\r\n", ($tk + 1), $totals);
+        $sql = $db->getCreateTableSql($tv) . ";\r\n\r\n";
+        file_put_contents($filename, $sql, FILE_APPEND);
+    }
+}
+exit('处理完成');
--- php/mysql.php
+++ php/mysql.php
@@ -24,37 +24,39 @@ class MySqlDB {
     /**
      * 初始化数据库链接标识
      *
-     * @param string $conf['host'] 地址
-     * @param string $conf['user'] 账号
-     * @param string $conf['pwd'] 密码
-     * @param string $conf['dbname'] 数据库名
+     * @param string $conf['hostname'] 地址
+     * @param string $conf['username'] 账号
+     * @param string $conf['password'] 密码
+     * @param string $conf['database'] 数据库名
+     * @param string $conf['hostport'] 端口
      * 
      * @return void
      */
     private function __construct($conf)
     {
-        $this->conn = new mysqli($conf['host'], $conf['user'], $conf['pwd'], $conf['dbname']);
+        $hostport = isset($conf['hostport']) && $conf['hostport'] ? $conf['hostport'] : '3306';
+        $this->conn = new mysqli($conf['hostname'], $conf['username'], $conf['password'], $conf['database'], $hostport);
     }
 
 	/**
      * 获取类实例
      *
-     * @param string $conf['host'] 地址
-     * @param string $conf['user'] 账号
-     * @param string $conf['pwd'] 密码
-     * @param string $conf['dbname'] 数据库名
+     * @param string $conf['hostname'] 地址
+     * @param string $conf['username'] 账号
+     * @param string $conf['password'] 密码
+     * @param string $conf['database'] 数据库名
      * @param boolean $is_single 是否单例(true:是 false:否)
      * @return object
      */
 	public static function getInstance($conf, $is_single = true){
         if ($is_single) {
-            if(self::$instance == null){
-                self::$instance = new self($conf);
+            if (!isset(self::$instance[$conf['database']]) || self::$instance[$conf['database']] == null) {
+                self::$instance[$conf['database']] = new self($conf);
             }
         } else {
-            self::$instance = new self($conf);
+            self::$instance[$conf['database']] = new self($conf);
         }
-		return self::$instance;
+		return self::$instance[$conf['database']];
     }
     
     /**
@@ -88,6 +90,20 @@ class MySqlDB {
         }
         return $field_arr;
     }
+
+    /**
+     * 获取创建表SQL语句
+     * 
+     * @param string $tb_name 表名
+     * 
+     * @return string
+     */
+    public function getCreateTableSql($tb_name) {
+        $query = $this->conn->query("SHOW CREATE TABLE {$tb_name}");
+        $result = $query->fetch_assoc();
+
+        return $result['Create Table'];
+    } 
 
     /**
      * 关闭数据库连接
